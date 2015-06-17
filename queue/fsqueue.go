af2172123c22a5a77d77eb9f35c21b6a17b877a0
inge4pres <fgualazzi@gmail.com>
2015-06-17 16:03:16 +0200

    Adding test for fsqueue

--- queue/fsqueue.go
+++ queue/fsqueue.go
@@ -1,54 +1,80 @@
 package gmq
 
 import (
-	"log"
+	"bufio"
+	"fmt"
 	"os"
 	"sync"
 )
 
-var l *log.Logger
-
-func init() {
-	l = log.New(os.Stdout, "GMQ FS ", log.LstdFlags|log.Ldate|log.Ltime)
-}
-
 type FsQueue struct {
 	lock       sync.RWMutex
 	Name, Path string
-	Topic      []string
 	File       *os.File
-	QObj       map[int][]byte
 }
 
 type FsPrioQueue struct {
+	lock       sync.RWMutex
 	Name, Path string
-	Topic      []string
 	File       *os.File
-	QObj       map[int][]byte
 }
 
-func (fs *FsQueue) getQueueFile() (f *os.File, err error) {
-	//TODO
-	return nil, nil
+func createQueueFile(fs *FsQueue) (err error) {
+	fs.File, err = os.Create(fs.Path + fs.Name)
+	return
 }
 
-func (fs *FsQueue) createQueueFile() (*os.File, error) {
-	return os.OpenFile(fs.Path+fs.Name+"_"+string(len(fs.QObj)), os.O_RDWR, 0660)
-
+func getQueueFile(fs *FsQueue) (err error) {
+	fs.File, err = os.OpenFile(fs.File.Name(), os.O_RDONLY, 660)
+	return
 }
 
-func (fs *FsQueue) Push() (err error) {
-	//	fs.File, err = fs.createQueueFile()
-	//	fs.File.Write()
-	//TODO
-	return nil
+func appendQueueFile(fs *FsQueue) (err error) {
+	fs.File, err = os.OpenFile(fs.Path+fs.Name, os.O_APPEND, 660)
+	return
+}
+
+func (fs *FsQueue) Push(o []byte) (err error) {
+	if err = appendQueueFile(fs); err != nil {
+		fmt.Println("Creating an empty file")
+		err = createQueueFile(fs)
+	}
+	defer fs.File.Close()
+	fs.lock.Lock()
+	fs.File.Write(o)
+	fs.File.WriteString("\n")
+	fs.File.Sync()
+	fs.lock.Unlock()
+	return
 }
 
 func (fs *FsQueue) Pop() []byte {
-	//TODO
-	return nil
+	fs.lock.Lock()
+	var ret []byte
+	if err := getQueueFile(fs); err != nil {
+		panic(err)
+	}
+	defer fs.File.Close()
+	firstline, err := bufio.NewReader(fs.File).ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	ret = []byte(firstline)
+	fs.sync()
+	fs.lock.Unlock()
+	return ret
 }
 
 func (fs *FsQueue) sync() {
-	//TODO
+	var lines []string
+	scanner := bufio.NewScanner(fs.File)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	createQueueFile(fs)
+	for n := 1; n < len(lines); n++ {
+		fs.File.WriteString(lines[n])
+		fs.File.WriteString("\n")
+	}
+	fs.File.Sync()
 }
