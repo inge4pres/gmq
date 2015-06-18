2394992097280791d879af0eaf3fbd5620c0a2af
francesco gualazzi <fgualazzi@gmail.com>
2015-06-18 15:58:54 +0200

    FS QUEUE test 50% coverage

--- queue/fsqueue.go
+++ queue/fsqueue.go
@@ -2,6 +2,7 @@ package gmq
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"sync"
@@ -25,7 +26,7 @@ func createQueueFile(fs *FsQueue) (err error) {
 }
 
 func getQueueFile(fs *FsQueue) (err error) {
-	fs.File, err = os.OpenFile(fs.File.Name(), os.O_RDONLY, 660)
+	fs.File, err = os.Open(fs.Path + fs.Name)
 	return
 }
 
@@ -36,7 +37,6 @@ func appendQueueFile(fs *FsQueue) (err error) {
 
 func (fs *FsQueue) Push(o []byte) (err error) {
 	if err = appendQueueFile(fs); err != nil {
-		fmt.Println("Creating an empty file")
 		err = createQueueFile(fs)
 	}
 	defer fs.File.Close()
@@ -66,15 +66,31 @@ func (fs *FsQueue) Pop() []byte {
 }
 
 func (fs *FsQueue) sync() {
-	var lines []string
-	scanner := bufio.NewScanner(fs.File)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	createQueueFile(fs)
-	for n := 1; n < len(lines); n++ {
-		fs.File.WriteString(lines[n])
-		fs.File.WriteString("\n")
-	}
+	//	var lines []string
+	//	scanner := bufio.NewReader(fs.File)
+	//	for line, err := scanner.ReadString('\n') {
+	//		lines = append(lines, line)
+	//		fmt.Printf("Reading %d lines from buffer", len(lines))
+	//	}
+	//	os.Remove(fs.File.Name())
+	//	fs.File = nil
+	//	if err := createQueueFile(fs); err != nil {
+	//		panic(err)
+	//	}
+	//	fmt.Println("Created new FILE and now starting write on it")
+	//	for n := 1; n < len(lines); n++ {
+	//		fs.File.WriteString(lines[n])
+	//		fmt.Printf("Line %d written on FILE %s", n, fs.File.Name())
+	//		fs.File.WriteString("\n")
+	//	}
+	//	fmt.Println("Closing FILE")
+	var current, final bytes.Buffer
+	read, _ := fs.File.Read(current.Bytes())
+	fmt.Printf("Read %d bytes", read)
+	off := bytes.IndexByte(current.Bytes(), '\n') + 1
+	n, _ := fs.File.ReadAt(final.Bytes(), int64(off))
+	fmt.Printf("Read %d bytes after offset", n)
+	fs.File.Write(final.Bytes())
+	fmt.Printf("BUFFER LENGHTS: current %d  ,  final %d", len(current.Bytes()), len(final.Bytes()))
 	fs.File.Sync()
 }
--- queue/fsqueue_test.go
+++ queue/fsqueue_test.go
@@ -1,42 +1,61 @@
 package gmq
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestcreateQueueFile(t *testing.T) {
-	q := &FsQueue{
-		Name: "queue_test_1",
-		Path: "./test/fs/",
-	}
-	err := createQueueFile(q)
-	if err != nil {
-		t.Errorf("Creation of file %s failed", q.Name+q.Path)
-	}
-}
+var message = []byte("TEST Message: you know, for testing...")
 
 func TestFsQueueFirstPush(t *testing.T) {
 	q := &FsQueue{
 		Name: "queue_test_1",
-		Path: "./test/fs/",
+		Path: "../test/fs/",
 	}
-	message := []byte("TEST Message: you know, for testing...")
 	err := q.Push(message)
 	if err != nil {
 		t.Errorf("Error: %T %s", err, err)
 	}
 }
 
+func TestFsQueueSequentialPush(t *testing.T) {
+	q := &FsQueue{
+		Name: "queue_test_2",
+		Path: "../test/fs/",
+	}
+	for i := 0; i < 10; i++ {
+		err := q.Push(message)
+		if err != nil {
+			t.Errorf("Error: %T %s", err, err)
+		}
+	}
+}
+
+//DOESN'T FAIL BUT SIDE-EFFECT IS PRESENT: must check goroutines concurrency pattern
 func TestFsQueueConcurrentPush(t *testing.T) {
 	q := &FsQueue{
-		Name: "queue_test_1",
-		Path: "./test/fs/",
+		Name: "queue_test_3",
+		Path: "../test/fs/",
 	}
-	message := []byte("TEST Message: you know, for testing...")
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		go func() {
 			err := q.Push(message)
 			if err != nil {
 				t.Errorf("Error: %T %s", err, err)
 			}
 		}()
+	}
+}
+
+func TestFsQueuePop(t *testing.T) {
+	q := &FsQueue{
+		Name: "queue_test_2",
+		Path: "../test/fs/",
+	}
+	ret := q.Pop()
+	// '\n' is a additive byte only used in Push()
+	if len(ret) != (len(message) + 1) {
+		t.Errorf("Message pop'd from queue incomplete! \n"+
+			"message: %d \n"+
+			"returned: %d", len(message), len(ret))
 	}
 }
