cee208a16058ae6b6bad22c599549cae68dc6543
francesco gualazzi <fgualazzi@gmail.com>
2015-06-18 15:58:23 +0200

    Just testing outside test pkg

--- gmq.go
+++ gmq.go
@@ -3,12 +3,13 @@ package main
 import (
 	_ "errors"
 	"flag"
-	_ "fmt"
+	"fmt"
 	q "gmq/queue"
 	"os"
 )
 
 var configfile string
+var message = []byte("TEST Message: you know, for testing...")
 
 func main() {
 
@@ -18,6 +19,25 @@ func main() {
 	//	if err := parseConfigFile(configfile); err != nil {
 	//		panic(errors.New("Unable to parse configuration file %s"))
 	//	}
+	qu := &q.FsQueue{
+		Name: "queue_test_2",
+		Path: "./test/fs/",
+	}
+	for i := 0; i < 10; i++ {
+		err := qu.Push(message)
+		if err != nil {
+			fmt.Printf("Error: %T %s", err, err)
+		}
+	}
+
+	//SECOND
+	ret := qu.Pop()
+	// '\n' is a additive byte only used in Push()
+	if len(ret) != (len(message) + 1) {
+		fmt.Printf("Message pop'd from queue incomplete! \n"+
+			"message: %d \n"+
+			"returned: %d", len(message), len(ret))
+	}
 }
 
 func parseConfigFile(f string) error {
