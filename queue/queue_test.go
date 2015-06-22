ece14a90dac58f9579bb18f4228ea08d701a1681
francesco gualazzi <fgualazzi@gmail.com>
2015-06-22 17:25:35 +0200

    Test for QUEUE added

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -2,11 +2,27 @@ package gmq
 
 import "testing"
 
-var (
-	message = []byte("TEST Message: you know, for testing..."),
-	queue = new(Queue{QName:"testqueue"})
-
+var message = []byte("TEST Message: you know, for testing...")
+var queue = &Queue{QName: "testqueue"}
 
 func TestQueueSimplePush(t *testing.T) {
-	
+	err := queue.Push(message)
+	if err != nil {
+		t.Errorf("Error %T %s", err, err)
+	}
+	if len(queue.QObj) != 1 {
+		t.Error("Push failed!")
+	}
+}
+
+func TestQueueSimplePop(t *testing.T) {
+	ret, err := queue.Pop()
+	if err != nil {
+		t.Errorf("Error %t %s", err, err)
+	}
+	if len(ret) != len(message) {
+		t.Errorf("Message pop'd from queue incomplete! \n"+
+			"message: %d \n"+
+			"returned: %d", len(message), len(ret))
+	}
 }
