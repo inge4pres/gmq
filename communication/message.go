af6ba549d2ca40d685c2d9fcc0f2659cef0ecaf7
francesco gualazzi <fgualazzi@gmail.com>
2015-06-26 13:51:56 +0200

    Added model

--- communication/message.go
+++ communication/message.go
@@ -6,11 +6,16 @@ import (
 
 type Message struct {
 	Operation byte   `json:"operation"`
+	Queue     string `json:"queue"`
 	Payload   []byte `json:"payload"`
-	Confirmed byte   `json:"confirm"`
+	Confirmed byte   `json:"confirmation"`
 }
 
 func ParseMessage(in []byte) (*Message, error) {
 	m := new(Message)
 	return m, json.Unmarshal(in, m)
+}
+
+func WriteMessage(m *Message) ([]byte, error) {
+	return json.Marshal(m)
 }
