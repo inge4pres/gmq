9302e63c0aa8bc98b8925721fcce26b7f992eca7
francesco gualazzi <fgualazzi@gmail.com>
2015-07-01 17:55:12 +0200

    Adapting to configuration

--- gmq.go
+++ gmq.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	_ "errors"
 	"flag"
-	_ "fmt"
-	_ "gmq/queue"
-	"os"
 )
 
 var configfile string
 
 func main() {
 
-	flag.StringVar(&configfile, "f", "gmq.conf", "Configuration file")
+	flag.StringVar(&configfile, "f", "/etc/gmq/gmq.json", "Configuration file")
 	flag.Parse()
 
 	//	if err := parseConfigFile(configfile); err != nil {
 	//		panic(errors.New("Unable to parse configuration file %s"))
 	//	}
-}
-
-func parseConfigFile(f string) error {
-	//TODO
-	_, err := os.Open(f)
-	return err
 }
