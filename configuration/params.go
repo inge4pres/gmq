367d5fb60ffe9412f3a960680947450f9e72d9bf
francesco gualazzi <fgualazzi@gmail.com>
2015-07-02 18:12:27 +0200

    Moved configuration methods to other pkg

--- configuration/params.go
+++ configuration/params.go
@@ -3,8 +3,6 @@ package gmqconf
 import (
 	"encoding/json"
 	"errors"
-	comm "gmq/communication"
-	q "gmq/queue"
 	"io/ioutil"
 )
 
@@ -27,54 +25,30 @@ type QConf struct {
 	MaxQueueC   int `json:"max_queue_capacity"`
 }
 
+type DbConf struct {
+	DbVendor string `json:"vendor"`
+	Dsn      string `json:"dsn"`
+}
+
+type FsConf struct {
+	Path string `json:"path"`
+}
+
 type Params struct {
 	Network NetConf `json:"network"`
 	Queue   QConf   `json:"queue"`
+	Db      DbConf  `json:"database"`
+	Fs      FsConf  `json:"filesystem"`
 }
 
-func parseConfiguration(file string) (*Params, error) {
+func ParseConfiguration(file string) (*Params, error) {
 	params := new(Params)
 	conf, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil, errors.New("Configuration file not available")
 	}
 	if err := json.Unmarshal(conf, params); err != nil {
-		return nil, errors.New("Unable to parse configuration file, check JSON is well formed")
+		return nil, err //errors.New("Unable to parse configuration file, check JSON is well formed")
 	}
 	return params, err
-}
-
-func ConfigureServer(conf *Params) *comm.Server {
-	s := new(comm.Server)
-	if conf.Network.Port == "" {
-		s.Port = comm.DEFAULT_LISTEN_PORT
-	}
-	if conf.Network.Proto == "" {
-		s.Proto = comm.DEFAULT_PROTOCOL
-	}
-	return s
-}
-
-func ConfigureQueue(conf *Params) (*q.QueueInterface, error) {
-	queue := new(q.QueueInterface)
-	if conf.Queue.MaxQueueN < 1 {
-		return nil, errors.New("Please configure MAX_QUEUE_NUMBER with a positive number")
-	}
-	if conf.Queue.MaxQueueC < 1 {
-		return nil, errors.New("Please configure MAX_QUEUE_CAPACITY with a positive number")
-	}
-	if conf.Queue.MaxMessageL < 1 {
-		return nil, errors.New("Please configure MAX_MESSAGE_LENGHT with a positive number")
-	}
-	switch conf.Queue.QueueType {
-	case USE_MEMORY:
-		queue := new(q.Queue)
-	case USE_DATABASE:
-		queue := new(q.DbQueue)
-	case USE_FILESYSTEM:
-		queue := new(q.FsQueue)
-	default:
-		return nil, errors.New("Please configure QUEUE_TYPE")
-	}
-	return queue, nil
 }
